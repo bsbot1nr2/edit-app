1dcef9ccadc55d27a1e8b05a4dc83ea1432e59c9
bsbot1nr2 <bsbot1nr2@gmail.com>
2025-12-16 11:04:02 +0100

    initial expo app

---
+++ app.js
@@ -0,0 +1,18 @@
+import { View, Text } from "react-native";
+
+export default function App() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "#000",
+        justifyContent: "center",
+        alignItems: "center"
+      }}
+    >
+      <Text style={{ color: "white", fontSize: 20 }}>
+        CapClone läuft 🚀
+      </Text>
+    </View>
+  );
+}
